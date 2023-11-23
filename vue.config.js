ba8ec08006574edc46e3165bc3f67536fba8c2d6
Célestin <hajatianajose.renabo@gmail.com>
2023-11-23 10:47:58 +0100

    Application web

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,14 @@
-const { defineConfig } = require('@vue/cli-service')
+// const { defineConfig } = require('@vue/cli-service')
+// module.exports = defineConfig({
+//   transpileDependencies: true
+// })
+
+const { defineConfig } = require('@vue/cli-service');
+
 module.exports = defineConfig({
-  transpileDependencies: true
-})
+  transpileDependencies: ['vuetify'], // Remplacez 'vuetify' par le nom de vos dépendances à transpiler
+  publicPath: process.env.NODE_ENV === 'production' ? '/Gestion-de-diplome/' : '/'
+});
+
+
+
